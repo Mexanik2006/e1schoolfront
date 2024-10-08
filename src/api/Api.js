330034bbdadbedb611c2eb97ed85134b68dc289d
Mexanik2006 <mexhanik2006@gmail.com>
2024-10-08 13:28:41 +0500

    api

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-// const Api = axios.create({
-//     baseURL: 'https://e1school-backend.onrender.com/'
-// })
 const Api = axios.create({
-    baseURL: 'http://localhost:2600/'
+    baseURL: 'https://e1school-backend.onrender.com/'
 })
+// const Api = axios.create({
+//     baseURL: 'http://localhost:2600/'
+// })
 
 export default Api
 
